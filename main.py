0bba8498dd1f390a2407c792fb61c4a824109041
rasdid43-web <rasdid43@gmail.com>
2026-07-17 04:40:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = "@Rr147258-"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🤖 أهلاً بك في MarketMasterBot\n\n"
+        "الأوامر:\n"
+        "/gold - تحليل الذهب\n"
+        "/crypto - تحليل العملات الرقمية\n"
+        "/forex - تحليل الفوركس"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
